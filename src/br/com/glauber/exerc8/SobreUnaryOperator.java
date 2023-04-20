815d106647d58575bc1f5cb0290c516a2bb49018
glauber <glaubero.souza@gmail.com>
2023-04-20 20:26:06 -0300

    Lambda e tratamento de erro exercícios: 8 e 9.

---
+++ src/br/com/glauber/exerc8/SobreUnaryOperator.java
@@ -0,0 +1,15 @@
+package br.com.glauber.exerc8;
+
+import java.util.function.UnaryOperator;
+
+public class SobreUnaryOperator {
+    public static void main(String[] args) {
+        UnaryOperator<Integer> somarCom2 = valor -> valor +2;
+        UnaryOperator<Integer> multiplicarPor2 = valor -> valor * 2;
+        UnaryOperator<Integer> aoQuadrado = valor -> valor * valor;
+        var resultado = somarCom2.andThen(multiplicarPor2).andThen(aoQuadrado).apply(10);
+        var resultadoCompose = somarCom2.compose(multiplicarPor2).compose(aoQuadrado).apply(10);
+        System.out.println("A operação de (+), (*) e (²) é:  " + resultado);
+        System.out.println("A operação de (+), (*) e (²) é:  " + resultadoCompose);
+    }
+}
---
+++ src/br/com/glauber/exerc9/SobreBinaryOperator.java
@@ -0,0 +1,22 @@
+package br.com.glauber.exerc9;
+
+import java.util.function.BiFunction;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+
+public class SobreBinaryOperator {
+    public static void main(String[] args) {
+        BinaryOperator<Double> calcularMedia = (nota1, nota2) -> (nota1 + nota2) / 2;
+        var media = calcularMedia.apply(8.0, 7.0) ;
+        System.out.println("A sua média é: " + media);
+
+        BiFunction<Double, Double, String> verificarAprovacao = (nota1, nota2) -> ((nota1 + nota2) / 2)
+                >= 6 ? "Foi Aprovado" : "Foi Reprovado";
+        System.out.println(verificarAprovacao.apply(8.0,8.0));
+
+        Function<Double, String> verificarAprovacaoo = valor -> valor >= 6 ? "Foi Aprovado" : "Foi Reprovado";
+        System.out.println(
+                calcularMedia.andThen(verificarAprovacaoo).apply(8.0,8.0)
+        );
+    }
+}
